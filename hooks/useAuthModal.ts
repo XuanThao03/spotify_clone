0a90141f77c43c9ad7a0e8ed70edbebdc5bba49e
XuanThao <vxuanthao03@gmail.com>
2024-07-11 00:52:57 +0700

    update login sign up by email and github

---
+++ hooks/useAuthModal.ts
@@ -0,0 +1,17 @@
+import exp from "constants";
+import { DefaultSerializer } from "v8";
+import { create } from "zustand";
+
+interface AuthModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useAuthModal = create<AuthModalProps>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useAuthModal;
